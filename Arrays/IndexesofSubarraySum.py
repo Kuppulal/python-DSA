e04f1cbe14218b09533e41252a552d3ed177217c
Kuppulal <laldev2112@gmail.com>
2026-06-27 19:57:54 +0530

    Add Python DSA solutions

---
+++ Arrays/IndexesofSubarraySum.py
@@ -0,0 +1,29 @@
+class solution:
+    def subarray(self, arr, target):
+        start = 0
+        curr_sum = 0
+
+        for end in range(len(arr)):
+            curr_sum += arr[end]
+
+            while curr_sum > target and start <= end:
+                curr_sum -= arr[start]
+                start += 1
+
+            if curr_sum == target:
+                return [start + 1, end + 1]
+
+        return [-1]
+
+
+if __name__ == "__main__":
+    arr = [1,2,3,7,5]
+    target = 12
+    obj = solution()
+    ans = obj.subarray(arr, target)
+
+    if ans == -1:
+        print(-1)
+    else:
+        print(ans[0],ans[1])
+        
---
+++ REDME.md
@@ -0,0 +1,10 @@
+# Python DSA
+
+This repository contains my Python solutions for Data Structures and Algorithms.
+
+## Topics
+- Arrays
+
+
+## Language
+- Python 3
